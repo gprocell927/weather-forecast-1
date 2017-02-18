d1964ea03ac1bc51814f82310b63d92bf680d766
Gabi Procell <gprocell927@gmail.com>
2017-02-17 18:13:39 -0700

    deleted code that was no longer needed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,9 +4,9 @@ import moment from 'moment'
 
 class App extends Component {
   render() {
-    const { times } = this.props
-    const d = new Date()
-    const h = d.getHours() // returns a number
+    // const { times } = this.props
+    // const d = new Date()
+    // const h = d.getHours() // returns a number
     //if the current hour is < 12 or > sunrise, display morning
     // if the current hour is >= 12 or < sunset, display afternoon
     // if the current hour is >= sunset or < sunset + 3 , display evening.
--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import Routes from './routes'
 import store from './store'
 
 store.dispatch((dispatch) => {
-  dispatch({type: 'FETCH_LOCAL_WEATHER_START'})
 
   const toMoment = utcTime => moment.utc(utcTime, "hh:mm:ss a").local()
 
@@ -19,28 +18,32 @@ store.dispatch((dispatch) => {
       axios
         .get(`http://api.wunderground.com/api/58583248c38e2876/conditions/q/${latitude},${longitude}.json`)
           .then((res) => res.data.current_observation)
-          .then((res) => ({                  currentTemp: res.temp_f,
-          currentWeather: res.weather
+          .then((res) => ({
+            currentTemp: res.temp_f,
+            currentWeather: res.weather
           }))
-          .then((weatherInfo) => dispatch({ type: 'SET_CURRENT_LOCAL_WEATHER', weatherInfo }))
+          .then((weatherInfo) => dispatch({
+            type: 'SET_CURRENT_LOCAL_WEATHER', weatherInfo
+          }))
 
 
       axios
-        .get(`http://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&date=today`)
+        // .get(`http://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&date=today`)
         .then((res) => res.data.results)
-        .then((res) => ({ sunrise: res.sunrise, sunset: res.sunset }))
+        .then((res) => ({
+          sunrise: res.sunrise,
+          sunset: res.sunset
+        }))
         .then(({ sunrise, sunset }) => ({
           sunrise: toMoment(sunrise).format("h:mm A"),
           sunset: toMoment(sunset).format("h:mm A")
         }))
-        .then((times) => dispatch({ type: 'SET_SUNRISE_SUNSET', times }))
+        .then((times) => dispatch({
+          type: 'SET_SUNRISE_SUNSET', times
+        }))
     })
-
-
-  //do something async
-  dispatch({type: 'BAR'})
-
 })
+
 render(
   <Provider store={store}>
     <Routes history={browserHistory} />
