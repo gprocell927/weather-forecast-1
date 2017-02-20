764c084f9cca244e2b0bf48086ba4a75f0031e28
Gabi Procell <gprocell927@gmail.com>
2017-02-20 16:22:45 -0700

    creates a settings route

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Router, Route } from 'react-router';
+import React from 'react'
+import { Router, Route } from 'react-router'
 
-import App from './containers/App';
+import App from './containers/App'
+import Settings from './containers/Settings'
 
 const Routes = (props) => (
   <Router {...props}>
     <Route path="/" component={App} />
+    <Route path="/settings" component={Settings} />
   </Router>
-);
+)
 
-export default Routes;
+export default Routes
