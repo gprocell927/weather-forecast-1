0a90b0e707353a55fd78a3ac63c3e107bd87f9e7
Gabi Procell <gprocell927@gmail.com>
2017-02-20 17:00:07 -0700

    adds settings presentational component with handleSubmit callback and cities props

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import times from './timesReducer'
 import weatherInfo from './currentLocalWeatherReducer'
+import pinnedCities from './settingsReducer'
 
 const rootReducer = combineReducers({
   times,
-  weatherInfo
+  weatherInfo,
+  pinnedCities
 })
 
 export default rootReducer
